fed2d538f118818e0cb31e1a16df95b67ac55e36
bcastro23 <bernal.castro@gmail.com>
2021-11-16 16:15:15 -0600

    Step 8 - added Translatable texts i18n*

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,9 +1,10 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
   "sap/m/MessageToast",
-  "sap/ui/model/json/JSONModel"
+  "sap/ui/model/json/JSONModel",
+  "sap/ui/model/resource/ResourceModel"
 
-], function (Controller, MessageToast, JSONModel) {
+], function (Controller, MessageToast, JSONModel, ResourceModel) {
 
   "use strict";
   return Controller.extend("sap.ui.demo.walkthrough.controller.App",{
@@ -16,11 +17,24 @@ sap.ui.define([
       };
       var oModel = new JSONModel(oData);
       this.getView().setModel(oModel);
+
+      //set i18n model on view
+      var i18nModel = new ResourceModel({
+        bundleName: "sap.ui.demo.walkthrough.i18n/i18n_en"
+      });
+      this.getView().setModel(i18nModel, "i18n_en");
     },    
     onShowHello : function () {
       //show native JS alert
       //alert("Hello World from a Controller");
-      MessageToast.show("Hello World from MessageToast");
+      //MessageToast.show("Hello World from MessageToast");
+
+      //read msg from i18n model
+      var oBundle = this.getView().getModel("i18n_en").getResourceBundle();
+      var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+      var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+      // show message
+      MessageToast.show(sMsg);
     }
   });
 });

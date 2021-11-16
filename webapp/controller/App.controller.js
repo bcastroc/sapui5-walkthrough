f4cf4a21c4398e411caf605cbca6107f3a70d2c0
bcastro23 <bernal.castro@gmail.com>
2021-11-16 15:03:36 -0600

    Step 6 - Added Module /MessageToast/

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,15 +1,15 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function (Controller) {
+  "sap/ui/core/mvc/Controller",
+  "sap/m/MessageToast"
+
+], function (Controller, MessageToast) {
 
   "use strict";
   return Controller.extend("sap.ui.demo.walkthrough.controller.App",{
-
     onShowHello : function () {
       //show native JS alert
-      alert("Hello World from a Controller");
+      //alert("Hello World from a Controller");
+      MessageToast.show("Hello World from MessageToast");
     }
-
   });
-
 });

3aeae8676d7ea29a59397746f7d6a7c0e0331a8a
bcastro23 <bernal.castro@gmail.com>
2021-11-18 15:53:58 -0600

    Step 9 - Add component configuration to project

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,29 +1,10 @@
 sap.ui.define([
   "sap/ui/core/mvc/Controller",
-  "sap/m/MessageToast",
-  "sap/ui/model/json/JSONModel",
-  "sap/ui/model/resource/ResourceModel"
+  "sap/m/MessageToast"
 
-], function (Controller, MessageToast, JSONModel, ResourceModel) {
-
+], function (Controller, MessageToast) {
   "use strict";
-  return Controller.extend("sap.ui.demo.walkthrough.controller.App",{
-    onInit : function () {
-      //set data model on view
-      var oData = {
-        recipient : {
-          name : "JSONModel World"
-        }
-      };
-      var oModel = new JSONModel(oData);
-      this.getView().setModel(oModel);
-
-      //set i18n model on view
-      var i18nModel = new ResourceModel({
-        bundleName: "sap.ui.demo.walkthrough.i18n/i18n_en"
-      });
-      this.getView().setModel(i18nModel, "i18n_en");
-    },    
+  return Controller.extend("sap.ui.demo.walkthrough.controller.App",{    
     onShowHello : function () {
       //show native JS alert
       //alert("Hello World from a Controller");
